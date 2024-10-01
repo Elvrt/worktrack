c717e404b89867520ebd5f46e6d3eec5b4238815
andybima6 <andybima6@gmail.com>
2024-10-02 00:28:05 +0700

    home page clock in

--- lib/main.dart
+++ lib/main.dart
@@ -90,8 +90,11 @@ class HomeScreen extends StatelessWidget {
               children: [
                 InkWell(
                   onTap: () {
-                    // Implementasi fungsi clock in
-                    _showMessage(context, 'Clock In berhasil');
+                    // Navigasi ke halaman ClockInPage
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ClockInPage()),
+                    );
                   },
                   child: CircleAvatar(
                     radius: 74,
@@ -137,48 +140,30 @@ class HomeScreen extends StatelessWidget {
               style: TextStyle(fontSize: 16, color: Colors.grey),
             ),
             SizedBox(height: 20),
-            // Clock In / Debug iOS App / Clock Out Buttons
+            // Clock In / Debug iOS App / Clock Out
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                InkWell(
-                  onTap: () {
-                    // Implementasi fungsi Clock In
-                    _showMessage(context, 'Clock In ditekan');
-                  },
-                  child: Column(
-                    children: [
-                      Icon(Icons.login, size: 40, color: Colors.yellow),
-                      SizedBox(height: 5),
-                      Text('Clock In', style: TextStyle(fontSize: 16)),
-                    ],
-                  ),
+                Column(
+                  children: [
+                    Icon(Icons.login, size: 40, color: Colors.yellow),
+                    SizedBox(height: 5),
+                    Text('Clock In', style: TextStyle(fontSize: 16)),
+                  ],
                 ),
-                InkWell(
-                  onTap: () {
-                    // Implementasi fungsi Debug iOS App
-                    _showMessage(context, 'Debug iOS App ditekan');
-                  },
-                  child: Column(
-                    children: [
-                      Icon(Icons.flag, size: 40, color: Colors.yellow),
-                      SizedBox(height: 5),
-                      Text('Debug iOS App', style: TextStyle(fontSize: 16)),
-                    ],
-                  ),
+                Column(
+                  children: [
+                    Icon(Icons.flag, size: 40, color: Colors.yellow),
+                    SizedBox(height: 5),
+                    Text('Debug iOS App', style: TextStyle(fontSize: 16)),
+                  ],
                 ),
-                InkWell(
-                  onTap: () {
-                    // Implementasi fungsi Clock Out
-                    _showMessage(context, 'Clock Out ditekan');
-                  },
-                  child: Column(
-                    children: [
-                      Icon(Icons.logout, size: 40, color: Colors.yellow),
-                      SizedBox(height: 5),
-                      Text('Clock Out', style: TextStyle(fontSize: 16)),
-                    ],
-                  ),
+                Column(
+                  children: [
+                    Icon(Icons.logout, size: 40, color: Colors.yellow),
+                    SizedBox(height: 5),
+                    Text('Clock Out', style: TextStyle(fontSize: 16)),
+                  ],
                 ),
               ],
             ),
@@ -256,3 +241,114 @@ class HomeScreen extends StatelessWidget {
     return "0$n";
   }
 }
+
+// Halaman baru untuk Clock In
+class ClockInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.all(16.0), // Add padding here
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                _formatTime(DateTime.now()),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                _formatDate(DateTime.now()),
+                style: TextStyle(fontSize: 15, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Center(
+                child: Text(
+                  'Clock In',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Your Location',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                maxLines: 4,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Location: Unknown - Khalid',
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Goal',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Debug ios app',
+                ),
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    // Implement Clock In functionality here
+                  },
+                  child: Text(
+                    'Clock In',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String _formatTime(DateTime dateTime) {
+    return DateFormat('HH:mm').format(dateTime);
+  }
+
+  String _formatDate(DateTime dateTime) {
+    return DateFormat('EEEE, MMM d').format(dateTime);
+  }
+}
+
